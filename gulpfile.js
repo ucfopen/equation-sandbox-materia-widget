e04d3b45e3bcddc211898b6de1c30506c20eb68b
Adrian Goetz <ad808102@cdl215380.local>
2016-03-28 13:38:08 -0400

    rearranged files locations to work with materia. Set output files of gulp command to assets/

--- gulpfile.js
+++ gulpfile.js
@@ -11,65 +11,65 @@ gulp.task('peg', shell.task([
 	// -e option sets the exported variable from modules.exports to window.latexParser
 	// --cache option turns on memoization, without it expressions like "\sin()" can
 	// hang the browser really bad (see https://github.com/pegjs/pegjs/issues/205)
-	'./node_modules/pegjs/bin/pegjs -e window.latexParser --cache ./src/peg/latex.peg ./js/latex.js'
+	'./node_modules/pegjs/bin/pegjs -e window.latexParser --cache ./gulp_this/src/peg/latex.peg ./assets/js/latex.js'
 ]));
 
 gulp.task('coffee', function() {
 	return gulp
-		.src('./src/coffee/*.coffee')
+		.src('./gulp_this/src/coffee/*.coffee')
 		.pipe(coffee({bare: true}).on('error', gutil.log))
-		.pipe(gulp.dest('./js/'))
+		.pipe(gulp.dest('./assets/js/'))
 });
 
 gulp.task('sass', function() {
 	return gulp
-		.src('./src/sass/*.scss')
+		.src('./gulp_this/src/sass/*.scss')
 		.pipe(sass())
-		.pipe(gulp.dest('./css/'));
+		.pipe(gulp.dest('./assets/css/'));
 });
 
 gulp.task('creator-css', ['sass'], function() {
 	return gulp
-		.src(['./css/creator.css'])
+		.src(['./gulp_this/css/creator.css'])
 		.pipe(concat('./creator.css'))
-		.pipe(gulp.dest('./stylesheets/'));
+		.pipe(gulp.dest('./assets/stylesheets/'));
 });
 
 gulp.task('player-css', ['sass'], function() {
 	return gulp
-		.src(['./css/player.css'])
+		.src(['./gulp_this/css/player.css'])
 		.pipe(concat('./player.css'))
-		.pipe(gulp.dest('./stylesheets/'));
+		.pipe(gulp.dest('./assets/stylesheets/'));
 });
 
 gulp.task('creator-js', ['coffee', 'peg'], function() {
 	return gulp
-		.src(['js/_bootstrap.js', 'js/latex.js', 'js/creator.js'])
+		.src(['./gulp_this/js/_bootstrap.js', 'gulp_this/js/latex.js', 'gulp_this/js/creator.js'])
 		.pipe(concat('creator.js'))
-		.pipe(gulp.dest('./scripts/'))
+		.pipe(gulp.dest('./assets/scripts/'))
 });
 
 gulp.task('creator-min-js', ['creator-js'], function() {
 	return gulp
-		.src('./scripts/creator.js')
+		.src('./gulp_this/scripts/creator.js')
 		.pipe(uglify())
 		.pipe(concat('creator.min.js'))
-		.pipe(gulp.dest('./scripts/'))
+		.pipe(gulp.dest('./assets/scripts/'))
 });
 
 gulp.task('player-js', ['coffee', 'peg'], function() {
 	return gulp
-		.src(['js/_bootstrap.js', 'js/latex.js', 'js/player.js'])
+		.src(['./js/_bootstrap.js', 'gulp_this/js/latex.js', 'gulp_this/js/player.js'])
 		.pipe(concat('player.js'))
-		.pipe(gulp.dest('./scripts/'))
+		.pipe(gulp.dest('./assets/scripts/'))
 });
 
 gulp.task('player-min-js', ['player-js'], function() {
 	return gulp
-		.src('./scripts/player.js')
+		.src('./gulp_this/scripts/player.js')
 		.pipe(uglify())
 		.pipe(concat('player.min.js'))
-		.pipe(gulp.dest('./scripts/'))
+		.pipe(gulp.dest('./assets/scripts/'))
 });
 
 gulp.task('default', ['build']);
